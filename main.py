8e7c144ee6497436a5c532a12226260baaa736b0
Sarang Balakrishnan <159267608+sarangkrish27@users.noreply.github.com>
2025-04-18 21:02:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import pyttsx3
+import speech_recognition as sr
+from google import genai
+client = genai.Client(api_key="Your API Key")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        return query
+
+    except Exception as e:
+        print(e)
+        print('Say that again please...')
+        return 'None'
+    
+def speak(audio):
+    engine = pyttsx3.init()
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def generate_response(prompt):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt,
+    )
+
+    return response.text
+
+
+input = ''
+speak("Hello, I am Jarvis. How can I help you today?")
+while input != 'exit':
+    input = takeCommand()
+    print(f'User: {input}')
+    print('Processing')
+    response = generate_response(input)
+    print(f'Jarvis: {response}')
+    speak(response)
